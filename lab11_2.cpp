6c647f4ea08ed64e05b44eccfe3c4ab9c93b9812
Pooh <tatakornaow@gmail.com>
2023-01-03 15:44:56 +0700

    lab11-2 edited

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -4,13 +4,17 @@
 using namespace std;
 
 int main (){
+    string boom;
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest << "-------------------- BOOM ---------------------"<<endl;
+	while (getline(source,boom)){
+	    dest << boom << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
